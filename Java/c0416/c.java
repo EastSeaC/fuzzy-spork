cd32d5525a1985eda7d30c95a825027c7bb1284e
吕德洪 <a815208129@163.com>
2021-04-13 19:36:52 +0800

    commit on master

--- Java/c0416/c.java
+++ Java/c0416/c.java
@@ -1,8 +1,10 @@
 package Java.c0416;
 
+import java.util.Scanner;
+
 public class c {
     public static void main(String[] args) {
         System.out.println();
-        
+        Scanner scanner = new Scanner(System.in);
     }
 }
